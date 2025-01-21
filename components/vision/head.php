376428424215c43521c9390de8125842d4b83c76
amili-code <amiralighorbani.info@gmail.com>
2025-01-21 16:14:59 +0330

    vision stractoure is loading

---
+++ components/vision/head.php
@@ -0,0 +1,22 @@
+    <div class="page-title">
+
+<div class="heading">
+    <div class="container">
+        <div class="row d-flex justify-content-center text-center">
+        <div class="col-lg-8">
+            <br>
+            <h1>چشم انداز تینتک</h1>
+            <p class="mb-0">شما می تونین با این صفحه از چشم انداز و آرمان باشگاه با خبر بشین</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    <nav class="breadcrumbs">
+        <div class="container">
+        <ol>
+            <li><a href="index.html">صفحه اصلی</a></li>
+            <li class="current">چشم انداز</li>
+        </ol>
+        </div>
+    </nav>
+    </div>
--- page-vision.php
+++ page-vision.php
@@ -2,7 +2,8 @@
 $inDir = get_template_directory_uri();
 get_header();
 
-// get_template_part('components/vision/cont');
-// get_template_part('components/vision/vision');
+get_template_part('components/vision/head');
+
+
 get_footer();
 ?>
